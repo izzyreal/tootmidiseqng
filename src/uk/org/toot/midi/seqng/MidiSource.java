9b6bff09e004d979f59083b15176f4ca6602b4b0
TootSoftware <TootSoftware@bf57f906-d9bb-11de-ad86-bdb49d26ad51>
2009-11-25 13:34:41 +0000

    Document race conditions and outcomes.

--- src/uk/org/toot/midi/seqng/MidiSource.java
+++ src/uk/org/toot/midi/seqng/MidiSource.java
@@ -6,18 +6,40 @@ import java.util.Observable;
 import javax.sound.midi.MidiEvent;
 
 /**
- * A MidiSource is a composite MidiEvent iterator.
- * This is the contract required by a Player/Renderer to be able to use arbitrary track
- * based representations of music. Such representations maybe be edited whilst being played. 
- * In general the representation need not be known a priori, it could even be generated 
- * in real-time.
- * An implementation of this class can be properly decoupled from the underlying
- * representation. The implementation of this class should know about its underlying
- * representation, that representation should not know about this implementation.
- * Clients of this class need not know about any such representation or any specific
- * implementation of this class.
+ * A MidiSource is a composite MidiEvent iterator. This is the contract required
+ * by a Player/Renderer to be able to use arbitrary track based representations
+ * of music. Such representations may be edited whilst being played. In general
+ * the representation need not be known a priori, it could even be generated in
+ * real-time. An implementation of this class can be properly decoupled from the
+ * underlying representation. The implementation of this class should know about
+ * its underlying representation, that representation should not know about this
+ * implementation. Clients of this class need not know about any such
+ * representation or any specific implementation of this class.
+ * 
+ * There are 2 edge cases in iteration.
+ * 
+ * Assume we have notes X then Y and X has already been consumed such that
+ * peek() and next() would both return Y. At some point peek() will return Y and
+ * its tick will indicate that it is ready to play so we would proceed to obtain
+ * it with next(). If inbetween peek() and next() the model is altered such that
+ * note Z is inserted between X and Y there are two eventualities. next() will
+ * now return Z. It may be too late to play in which case it should be ignored
+ * but it may be that it should play in which case it will be played and peek()
+ * and next() would subsequently obtain note Y which next() was originally
+ * expecting. So in this case an apparent race condition with note insertion
+ * near the current iterator position is handled properly.
+ * 
+ * Again, assume we have notes X then Y then Z and X has already been consumed such that
+ * peek() and next() would both return Y. At some point peek() will return Y and
+ * its tick will indicate that it is ready to play so we would proceed to obtain
+ * it with next(). If inbetween peek() and next() the model is altered such that
+ * Y is deleted next() will return Z. It may be that Z is too early to be played
+ * in which case next() has consumed it too early and Z would not subsequently
+ * be played at the appropriate time. In this case the race condition with note deletion
+ * near the current iterator position is not handled properly and notes may be lost.
+ * 
  * @author st
- *
+ * 
  */
 public abstract class MidiSource extends Observable
 {
