e5f215e9b3fd324e43d517fd18821b83ca8c48da
TootSoftware <TootSoftware@bf57f906-d9bb-11de-ad86-bdb49d26ad51>
2009-11-25 12:37:43 +0000

    First prototype of the MidiSource contract which allows a Player/Renderer client to work with arbitrary music representations.

---
+++ src/uk/org/toot/midi/seqng/MidiSource.java
@@ -0,0 +1,40 @@
+package uk.org.toot.midi.seqng;
+
+import java.util.List;
+import java.util.Observable;
+
+import javax.sound.midi.MidiEvent;
+
+/**
+ * A MidiSource is a composite MidiEvent iterator.
+ * This is the contract required by a Player/Renderer to be able to use arbitrary track
+ * based representations of music. Such representations maybe be edited whilst being played. 
+ * In general the representation need not be known a priori, it could even be generated 
+ * in real-time.
+ * An implementation of this class can be properly decoupled from the underlying
+ * representation. The implementation of this class should know about its underlying
+ * representation, that representation should not know about this implementation.
+ * Clients of this class need not know about any such representation or any specific
+ * implementation of this class.
+ * @author st
+ *
+ */
+public abstract class MidiSource extends Observable
+{
+	public abstract List<TrackSource> getTrackSources();
+	
+	public interface TrackSource
+	{
+		/**
+		 * Return the next MidiEvent without changing iterator position.
+		 * @return the next MidiEvent
+		 */
+		public MidiEvent peek();
+		
+		/**
+		 * Return the next MidiEvent and increment iterator position.
+		 * @return the next MidiEvent
+		 */
+		public MidiEvent next();
+	}
+}
